ac734a7656b4a5bcc3bf376643cdde5b9af00117
Karan <Malhari@sheridancollege.ca>
2026-02-09 17:22:49 -0500

    Added documentation by Karandeep Singh Malhari

--- src/com/malhari/product/ProductManager.java
+++ src/com/malhari/product/ProductManager.java
@@ -5,6 +5,8 @@ public class ProductManager {
     public double getPriceAfterTax(double price, double taxRate) {
         return price + (price * taxRate);
     }
+ // Calculates the final price after applying a percentage discount.
+
     public double calculateDiscount(double price, double discountPercent) {
         return price - (price * (discountPercent / 100.0));
     }
